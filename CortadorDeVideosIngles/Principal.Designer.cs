2753d80dc60db2cd1c25e91c6f2fe0d339a94633
Mathias <mathiasmoreira@gmail.com>
2017-11-21 00:18:25 -0200

    commit inicial

---
+++ CortadorDeVideosIngles/Principal.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CortadorDeVideosIngles
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.timeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(32, 64);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(126, 64);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 23);
+            this.buttonPause.TabIndex = 1;
+            this.buttonPause.Text = "Pause";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(207, 64);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // trackBar
+            // 
+            this.trackBar.AutoSize = false;
+            this.trackBar.Location = new System.Drawing.Point(12, -1);
+            this.trackBar.Maximum = 1000;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(588, 44);
+            this.trackBar.TabIndex = 4;
+            this.trackBar.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Location = new System.Drawing.Point(334, 46);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(248, 17);
+            this.timeLabel.TabIndex = 5;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 355);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.buttonPlay);
+            this.Name = "Principal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonPause;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ CortadorDeVideosIngles/Principal.cs
@@ -0,0 +1,67 @@
+﻿using NAudio.Wave;
+using System;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace CortadorDeVideosIngles
+{
+    public partial class Principal : Form
+    {
+        IWavePlayer waveOutDevice;
+        AudioFileReader audioFileReader;
+
+        long audioPosition = 0;
+        int playerPosition = 0;
+
+        public Principal()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            waveOutDevice = new WaveOut();
+            audioFileReader = new AudioFileReader("C:\\Users\\Mathias\\Videos\\4K Video Downloader\\Does Drinking Alcohol Kill Your Gut Bacteria.mp3");
+            waveOutDevice.Init(audioFileReader);
+
+            waveOutDevice.Play();
+
+            var timer = new System.Timers.Timer();
+            timer.Interval = 300;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            playerPosition = Convert.ToInt32(decimal.Round(((audioFileReader.Position * 1000) / audioFileReader.Length)));
+
+            if (trackBar.InvokeRequired)
+            {
+                trackBar.BeginInvoke((MethodInvoker)delegate
+                {
+                    trackBar.Value = playerPosition;
+                });
+            }
+
+            if (timeLabel.InvokeRequired)
+            {
+                timeLabel.BeginInvoke((MethodInvoker)delegate
+                {
+                    timeLabel.Text = $" {audioFileReader.CurrentTime.Minutes}:{audioFileReader.CurrentTime.Seconds}/{audioFileReader.TotalTime.Minutes}:{audioFileReader.TotalTime.Seconds}";
+                });
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            waveOutDevice.Pause();
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            waveOutDevice.Stop();
+        }
+    }
+}
